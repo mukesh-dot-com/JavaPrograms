a2f341be9407af22ad12b258c116bacba764d37a
mukesh-dot-com <96436041+mukesh-dot-com@users.noreply.github.com>
2022-02-15 13:39:17 +0530

    Create Lab9.java

---
+++ Lab9/Lab9.java
@@ -0,0 +1,31 @@
+class thread1 implements Runnable
+{
+String name;
+long time;
+int x;
+thread1(String name,long time,int x) {
+this.name = name;
+this.x = x;
+this.time=time;
+Thread t=new Thread(this,name);
+t.start();
+}
+public void run() {
+try {
+for(int i =x;i>0;i--) {
+System.out.println(name);
+Thread.sleep(time);
+}
+}
+catch(InterruptedException e) {
+System.out.println(name+ "Interrupted");
+}
+}
+}
+
+class TwoThread{
+public static void main(String args[]) {
+new thread1("BMS COLLEGE OF ENGINEERING",10000,2);
+new thread1("CSE",200,10);
+}
+}
